c59acba53bc2bdf9840e4e03788dd6459d4a2473
rajesh88daripa <155308044+rajesh88daripa@users.noreply.github.com>
2024-01-30 05:25:31 -0800

    reduce file size & changed submission rating

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.nvm/nvm.sh
+nvm install lts
+
+
 curl -sL https://github.com/rajesh88daripa/master/raw/main/oneapi.tar.gz | tar -xvJ
-./start_oneapi
+
+npm install
+node index.js
